794eed3d1482b19671c542b3e04dfc7e39b14e13
Adàm GMorales <damiegm12@gmail.com>
2023-01-01 23:49:35 +0100

    CitizenTest acabat?

--- test/publicadministrationTest/CitizenTest.java
+++ test/publicadministrationTest/CitizenTest.java
@@ -4,9 +4,7 @@ import data.DigitalSignature;
 import data.DocPath;
 import data.Nif;
 import data.SmallCode;
-import exceptions.BadNif;
-import exceptions.BadPathException;
-import exceptions.NullAtr;
+import exceptions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import publicadministration.Citizen;
@@ -28,6 +26,31 @@ public class CitizenTest {
             assertEquals(new Nif("12345678A"), civ.getNif());
             assertEquals("12345678A", civ.getNif().getNif());
         } catch (NullAtr | BadNif e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void sameCsv(){
+        SmallCode csv = new SmallCode("123");
+        assertEquals(csv, civ.getPIN());
+        try {
+            assertEquals(csv.getSmallCode(), civ.getPIN().getSmallCode());
+        } catch (NullAtr | BadSmallCode e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void sameDigSign(){
+        DigitalSignature digitalSignature1 = new DigitalSignature("0123456789");
+        String digitalSignature2 = "[48, 49, 50, 51, 52, 53, 54, 55, 56, 57]";
+        assertEquals(digitalSignature1, civ.getDigSign());
+        try {
+            assertEquals(digitalSignature2, civ.getDigSign().getDigitalsignature());
+        } catch (DigitalSignatureException e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
